6bf550d5b6ed6a10349320e04519330746712d62
Paul Buser <buser.paul@gmail.com>
2016-09-10 19:01:46 -0700

    WIP

--- Axiom_49_61_Classic/__init__.py
+++ Axiom_49_61_Classic/__init__.py
@@ -1,7 +1,18 @@
-# Python bytecode 2.7 (62211) disassembled from Python 2.7
-# Embedded file name: /Users/versonator/Jenkins/live/output/mac_64_static/Release/python-bundle/MIDI Remote Scripts/Axiom_49_61_Classic/__init__.py
-# Compiled at: 2016-05-20 03:43:52
-from Axiom import Axiom
+from _Generic.GenericScript import GenericScript
+from _Framework.Capabilities import *
+import Live
+from config import *
 
 def create_instance(c_instance):
-    return Axiom(c_instance)
+    """ The generic script can be customised by using config.py."""
+    generic_script = GenericScript(c_instance,
+                         Live.MidiMap.MapMode.relative_binary_offset,
+                         Live.MidiMap.MapMode.absolute, DEVICE_CONTROLS,
+                         TRANSPORT_CONTROLS, VOLUME_CONTROLS, TRACKARM_CONTROLS,
+                         BANK_CONTROLS, CONTROLLER_DESCRIPTION)
+    generic_script._device_selection_follows_track_selection = True
+    return generic_script
+
+def get_capabilities():
+    return {CONTROLLER_ID_KEY: controller_id(vendor_id=9999, product_ids=[9999], model_name='Axiom 49'),
+            PORTS_KEY: [inport(props=[NOTES_CC, REMOTE, SCRIPT]), outport(props=[SCRIPT])]}
