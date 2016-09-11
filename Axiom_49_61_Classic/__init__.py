df4987972314045cb472cd823e140747a36f0f4a
Paul Buser <buser.paul@gmail.com>
2016-09-10 17:57:51 -0700

    Initial commit.

---
+++ Axiom_49_61_Classic/__init__.py
@@ -0,0 +1,7 @@
+# Python bytecode 2.7 (62211) disassembled from Python 2.7
+# Embedded file name: /Users/versonator/Jenkins/live/output/mac_64_static/Release/python-bundle/MIDI Remote Scripts/Axiom_49_61_Classic/__init__.py
+# Compiled at: 2016-05-20 03:43:52
+from Axiom import Axiom
+
+def create_instance(c_instance):
+    return Axiom(c_instance)
